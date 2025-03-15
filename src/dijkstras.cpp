bc6f33a2e98f6da2b9dc514c2037af85488e2719
max <mibader@uci.edu>
2025-03-15 01:19:41 +0000

    wrote extract shortest path and print path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -18,7 +18,6 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     int n = G.numVertices;
     vector<int> distance(n, INF);
     vector<bool> visited(n, false);
-    
     priority_queue<Node, vector<Node>, greater<Node>> pq;
     pq.push(Node(source, 0));
     distance[source] = 0;
@@ -28,14 +27,11 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
         Node current = pq.top();
         pq.pop();
         int u = current.vertex;
-
         if (visited[u]) continue;
         visited[u] = true;
-
         for (const Edge& neighbor : G[u]) {
             int v = neighbor.dst;
             int weight = neighbor.weight;
-
             if (!visited[v] && distance[u] + weight < distance[v]) {
                 distance[v] = distance[u] + weight;
                 previous[v] = u;
@@ -45,4 +41,27 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     }
 
     return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    if (distances[destination] == INF)
+        return path;
+    for (int v = destination; v != -1; v = previous[v])
+        path.push_back(v);
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+void print_path(const vector<int>& path, int total) {
+    if (path.empty()) {
+        cout << "No path found." << endl;
+        return;
+    }
+    cout << "Shortest path: ";
+    for (size_t i = 0; i < path.size(); ++i) {
+        cout << path[i];
+        if (i < path.size() - 1) cout << " -> ";
+    }
+    cout << " (Cost: " << total << ")" << endl;
 }
