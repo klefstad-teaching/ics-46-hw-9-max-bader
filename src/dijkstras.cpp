4fa2a7b636c9b960952dee697bbaf4114c64f39e
max <mibader@uci.edu>
2025-03-15 01:53:18 +0000

    fxed pritn path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -56,7 +56,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path found." << endl;
+        cout << "\nTotal cost is " << total << "\n";
         return;
     }
     for (size_t i = 0; i < path.size(); ++i)
